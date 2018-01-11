0d576cda28a9a3d84fb75649a80bef7ff61be8ea
RachetRalph <ralphandrade@zoho.com>
2018-01-11 14:42:39 -0700

    Added nessary variables

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env."lMllF70PPYxT8AYQPfVrMljsm",
-  consumer_secret: process.env."5i9W6CwjosMmMtf140E4cUhjj5OFgrlD9M4lkSDleASnY5C77d",
-  access_token_key: process.env."810961489511096320-A4Fko0iRl0iOPXYn0qcUg3O7qNnh4vu",
-  access_token_secret: process.env."uqckwu2lCJV5TRATUkpk79zZNsbCAnVejr8oUKtEJwSoy"
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-  id: process.env."9a03609f383944b5ab4da75feaf825e6",
-  secret: process.env."ab07c10322354e83842ab42194749cd3"
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -1,5 +1,10 @@
 require("dotenv").config();
-
-// Link to keys.js to access keys!
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+// NPM Modules used to power this application 
+var twitt = require("twitter");
+var omdb = require("request");
+var spotify = require ("spotify");
+// Loads the fs pkg to read and write to log.txt files
+var fs = require("fs");
+// Inline arguments the node application will take
+var command = process.argv[2];
+var value = process.argv[3]; 
