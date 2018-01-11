dd6efa33da07b214b8af612d85c8136be2da070b
RachetRalph <ralphandrade@zoho.com>
2018-01-11 12:27:21 -0700

    Added .env file

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env."lMllF70PPYxT8AYQPfVrMljsm",
+  consumer_secret: process.env."5i9W6CwjosMmMtf140E4cUhjj5OFgrlD9M4lkSDleASnY5C77d",
+  access_token_key: process.env."810961489511096320-A4Fko0iRl0iOPXYn0qcUg3O7qNnh4vu",
+  access_token_secret: process.env."uqckwu2lCJV5TRATUkpk79zZNsbCAnVejr8oUKtEJwSoy"
+};
+
+exports.spotify = {
+  id: process.env."9a03609f383944b5ab4da75feaf825e6",
+  secret: process.env."ab07c10322354e83842ab42194749cd3"
+};
